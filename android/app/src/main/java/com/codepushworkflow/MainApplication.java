666f275209693119b4c135c0f50559457c94d48c
dwicao <lutsoft@gmail.com>
2019-08-12 13:44:26 +0700

    Feat: implement CodePush

--- android/app/src/main/java/com/codepushworkflow/MainApplication.java
+++ android/app/src/main/java/com/codepushworkflow/MainApplication.java
@@ -3,6 +3,7 @@ package com.codepushworkflow;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -15,6 +16,11 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
+    protected String getJSBundleFile(){
+      return CodePush.getJSBundleFile();
+    }
+
+    @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
@@ -22,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
+import codePush from "react-native-code-push";
 import App from './App';
 
-AppRegistry.registerComponent('codepushworkflow', () => App);
+AppRegistry.registerComponent('codepushworkflow', () => codePush(App));
